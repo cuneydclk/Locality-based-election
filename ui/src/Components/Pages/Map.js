b3bcb27ddc4598ff68ad29ffa78346dea69d87f2
cuneydclk <cuneydclk@gmail.com>
2023-06-13 01:36:32 +0300

    Cüneyd Harita Etkileşimi
    Map renklendirildi ve etkileşime açık hale geldi.

--- ui/src/Components/Pages/Map.js
+++ ui/src/Components/Pages/Map.js
@@ -1,59 +1,97 @@
-
 import React, { useEffect } from 'react';
 import L from 'leaflet';
-
 import 'leaflet/dist/leaflet.css';
-
-
-
 
 function Map() {
   useEffect(() => {
-    // Create the map
-    const map = L.map('map').setView([38.3287, 26.7903], 13);
+    const map = L.map('map').setView([38.2987, 26.6803], 10);
 
-    // Add the tile layer to the map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; OpenStreetMap contributors',
     }).addTo(map);
 
     // Add event listener for neighborhood click
     function onNeighborhoodClick(e) {
-      // Display information about the clicked neighborhood
-      console.log('Neighborhood clicked:', e.target.feature.properties.name);
+      const clickedFeature = e.target.feature;
+      console.log('Neighborhood clicked:', clickedFeature.properties);
+
+      // Get the centroid of the clicked feature
+      const centroid = L.geoJSON(clickedFeature).getBounds().getCenter();
+
+      // Set the map view to the centroid
+      map.setView(centroid, 13);
     }
 
-    // Add your neighborhood data and style
-    const neighborhoodsData = [
-      {
-        type: 'Feature',
-        properties: {
-          name: 'Neighborhood 1',
-        },
-        geometry: {
-          type: 'Polygon',
-          coordinates: [[
-            [26.77, 38.33],
-            [26.78, 38.34],
-            [26.79, 38.32],
-            [26.77, 38.33],
-          ]],
-        },
-      },
-      // Add more neighborhoods here...
-    ];
+    // Fetch the GeoJSON file
+    fetch('/static/media/map.d229d9af4c3a35093e05.geojson')
+      .then(response => response.json())
+      .then(geojsonData => {
+        if (geojsonData && geojsonData.features) {
+          geojsonData.features.forEach((feature, index) => {
+            const neighborhoodLayer = L.geoJSON(feature, {
+              style: {
+                fillColor: getRandomColor(index),
+                fillOpacity: 0.95, // Adjust the transparency here
+                color: 'black',
+                weight: 1,
+              },
+              onEachFeature: (feature, layer) => {
+                layer.on('click', onNeighborhoodClick);
+              },
+            }).addTo(map);
+          });
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching GeoJSON data:', error);
+      });
 
-    const neighborhoodsLayer = L.geoJSON(neighborhoodsData, {
-      onEachFeature: (feature, layer) => {
-        layer.on('click', onNeighborhoodClick);
-      },
-    }).addTo(map);
-
-    // Clean up the map when the component is unmounted
-    return () => map.remove();
+    return () => {
+      // Clean up the map when the component unmounts
+      map.remove();
+    };
   }, []);
 
-  return <div id="map" style={{ height: '500px' }}></div>;
+  return <div id="map" style={{ height: '400px' }}></div>;
+}
+
+function getRandomColor(index) {
+  const hue = (index * 137.5) % 360; // Generate hue based on index
+  const saturation = 80; // Adjust saturation as needed (0-100)
+  const lightness = 50; // Adjust lightness as needed (0-100)
+
+  // Convert HSL color to RGB format
+  const hslToRgb = (h, s, l) => {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l;
+    } else {
+      const hueToRgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hueToRgb(p, q, h + 1 / 3);
+      g = hueToRgb(p, q, h);
+      b = hueToRgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  };
+
+  // Return the generated color
+  return hslToRgb(hue, saturation, lightness);
 }
 
 export default Map;
